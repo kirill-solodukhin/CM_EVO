79fdc0fc24c4f993889b980ecc0ad9456167e061
SoloduhinKYu <kirillsoloduhin072@gmail.com>
2025-07-22 08:43:19 +0300

    remake Lab_4_Lib

---
+++ LAB_4_Lib/src/main/java/Main.java
@@ -0,0 +1,10 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        ComplexNumber num_1 = new ComplexNumber(10, -11);
+        ComplexNumber num_2 = new ComplexNumber(10, -11);
+
+        System.out.println(num_1.add(num_2));
+    }
+}
