cb3ca43484282fc4e4d89df05cd67b2bc070dde3
SoloduhinKYu <kirillsoloduhin072@gmail.com>
2025-08-06 17:00:14 +0300

    added new pass modifier

---
+++ LAb_6_Security/src/main/java/org/example/EncodeDecode/PasswordModifier.java
@@ -0,0 +1,114 @@
+package org.example.EncodeDecode;
+
+import com.github.javakeyring.PasswordAccessException;
+import org.example.AuthorizationInfo;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.util.Arrays;
+
+public class PasswordModifier
+{
+    private byte[] salt = new byte[16];
+    private DataStorage dataStorage;
+
+    public PasswordModifier(DataStorage dataStorage)
+    {
+        this.dataStorage = dataStorage;
+    }
+
+    public void savePassword(String password) throws PasswordAccessException
+    {
+        generateSalt(); // new salt
+        password = hashingPassword(password);
+        dataStorage.saveStringData(password, TYPE.PASSWORD);
+    }
+
+    public boolean checkPassword(String Password)
+    {
+        // old salt
+        try
+        {
+            getSalt();
+
+            String savingHash = dataStorage.getStringData(TYPE.PASSWORD);
+            Password = hashingPassword(Password);
+
+            return savingHash.equals(Password);
+        }
+        catch (PasswordAccessException e)
+        {
+            throw new RuntimeException("Не должно быть проблем с доступом к паролю: " + e);
+        }
+    }
+
+    private String hashingPassword(String password)
+    {
+        try
+        {
+            MessageDigest digester = MessageDigest.getInstance("SHA-512");
+
+            byte[] bytePassword = password.getBytes();     // Пароль, который хэшируем
+            byte[] digest = digester.digest(bytePassword); // хэш
+
+            digester.update(salt);
+
+            return Arrays.toString(digest);
+        }
+        catch (NoSuchAlgorithmException e)
+        {
+            throw new RuntimeException("Не должно быть: { При хэшировании пароля }" + e.getMessage());
+        }
+    }
+
+    private void getSalt() throws PasswordAccessException
+    {
+        if(!Arrays.equals(salt, new byte[16]))
+        {
+            return;
+        }
+
+        AuthorizationInfo authorizationInfo = new AuthorizationInfo(dataStorage);
+        boolean authorizedFlag = authorizationInfo.isAuthorized();
+
+        String saltArr = dataStorage.getStringData(TYPE.SALT);
+        String[] strings = saltArr.substring(1, saltArr.length() - 1).split(",");
+        byte[] bytes = new byte[16];
+
+        for(int i = 0; i < 16; i++)
+        {
+            bytes[i] = Byte.parseByte(strings[i].trim());
+        }
+
+        salt = bytes;
+    }
+
+    private void generateSalt()
+    {
+        try
+        {
+            dataStorage.deleteStringData(TYPE.SALT);
+        }
+        catch (PasswordAccessException ignored)
+        {
+        }
+
+        try
+        {
+            salt = new byte[16];
+            SecureRandom.getInstanceStrong().nextBytes(salt);
+            dataStorage.saveStringData(Arrays.toString(salt), TYPE.SALT);
+        }
+        catch (PasswordAccessException e)
+        {
+            throw new RuntimeException("An attempt to get data that does not exist in " +
+                    "{ generateSalt after delete old salt }: " + e);
+        }
+        catch (NoSuchAlgorithmException e)
+        {
+            throw new RuntimeException(e);
+        }
+    }
+}
